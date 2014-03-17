f01fab46252543cf126b59e57a19688d2f72d5fb
zizhu <zizhu.hzb@taobao.com>
2014-03-17 10:53:18 +0800

    完成静态demo

--- 1.0/index.js
+++ 1.0/index.js
@@ -1,50 +1,79 @@
-/**
- * @fileoverview 
+/*
+ * @fileoverview
  * @author 子竹<zizhu.hzb@taobao.com>
  * @module rainlib
- **/
-KISSY.add(function (S, Node,Base, IO) {
-    var EMPTY = '';
-    var $ = Node.all;
-    /**
-     * 
-     * @class Rainlib
-     * @constructor
-     * @extends Base
-     */
-    function Rainlib(comConfig) {
-        var self = this;
-        var load = function(){
-          console.log(IO);
-        }
-        var error = function(){
+*/
 
-        }
-        self.setup  = function(id, userId){
-          return;
-          var bodyData = {};
-          bodyData["id"] = id;
-          bodyData["userId"] = userId;
-          new IO({
-            url:"",
-            type: "get",
-            dataType: "jsonp",
-            data:bodyData,
-            success: load,
-            error:error
-          });
-        }
-        //调用父类构造函数
-        Rainlib.superclass.constructor.call(self, comConfig);
+KISSY.add(function(S, Node, IO, Template) {
+  var $, Rainlib, loader, render, views;
+  $ = Node.all;
+  views = {};
+  views.draw = "<div class=\"rain-recom u3\">\n  <ul class=\"rain-left\">\n    <li><a href=\"\" target=\"_blank\"><img src=\"http://gtms01.alicdn.com/tps/i1/T1C1TmFkXaXXaC1Jrl-250-250.png\" alt=\"\">\n        <div class=\"rain-product-info\">\n          <div class=\"title\">测试</div>\n          <div class=\"price u-1-2\">$12.4</div>\n          <div class=\"special u-1-2\">$3.9</div>\n        </div></a></li>\n    {{#each items}}<li class=\"{{cls}}\"><a href=\"\" target=\"_blank\"><img src=\"http://gtms01.alicdn.com/tps/i1/T1C1TmFkXaXXaC1Jrl-250-250.png\" alt=\"\">\n        <div class=\"rain-product-info\">\n          <div class=\"title\">{{title}}</div>\n          <div class=\"price u-1-2\">{{price}}</div>\n          <div class=\"special u-1-2\">{{special}}</div>\n        </div></a></li>{{/each}}\n  </ul>\n</div>";
+  views.photo = "这里是相册组件";
+  views.recom = "推荐相册";
+  loader = function(sessionId, target) {
+    var error, param;
+    param = {};
+    param["id"] = sessionId;
+    error = function() {
+      console.log("发生异常，打点到其它服务器");
+      return target.hide();
+    };
+    return new IO({
+      url: "http://5aoa.cd",
+      type: "get",
+      dataType: "jsonp",
+      data: param,
+      fail: error,
+      success: function(data) {
+        return render(data, target);
+      }
+    });
+  };
+  render = function(data, target) {
+    var e, tpl;
+    tpl = views[data.name];
+    try {
+      render = new Template(tpl).render(data);
+      target.html(render);
+    } catch (_error) {
+      e = _error;
+      error();
+    }
+    return null;
+  };
+  Rainlib = (function() {
+    function Rainlib(targetId, session) {
+      self.target = Node.all("#" + targetId);
+      self.session = session;
     }
 
-    S.extend(Rainlib, Base, /** @lends Rainlib.prototype*/{
+    Rainlib.prototype.setup = function() {
+      var data, html, item;
+      item = {
+        cls: "small",
+        title: "测试",
+        price: 12.8,
+        specail: 18.0
+      };
+      data = {
+        cls: "test-cls",
+        items: [item, item, item, item, item, item]
+      };
+      render = new Template(views.draw).render(data);
+      self.target.html(render);
+      return;
+      if (typeof self.session === "object") {
+        return html = render(self.session, self.target);
+      } else {
+        return loader(self.session, self.target);
+      }
+    };
 
-    }, {ATTRS : /** @lends Rainlib*/{
-
-    }});
     return Rainlib;
-}, {requires:['node', 'base', 'io', 'gallery/rainlib/1.0/index.css']});
 
-
-
+  })();
+  return Rainlib;
+}, {
+  requires: ['node', 'io', 'xtemplate', 'gallery/rainlib/1.0/index.css']
+});
--- README.md
+++ README.md
@@ -4,10 +4,18 @@
 * 教程：[http://gallery.kissyui.com/rainlib/1.0/guide/index.html](http://gallery.kissyui.com/rainlib/1.0/guide/index.html)
 * demo：[http://gallery.kissyui.com/rainlib/1.0/demo/index.html](http://gallery.kissyui.com/rainlib/1.0/demo/index.html)
 
+## 使用说明
+  * 通过gallary引入rainlib组件
+  * var rain = new Rainlib()
+  * rain.setup(id, sessionid)
+  * id：需要渲染区块的ID编号
+  * sessionid：当前用户的登录id
+
 ## changelog
 
 ### V1.0
 
-    [!]
+    [!] 彩虹装修第一版发布，支持自定义区，图片混排，图片相册三大组件
+
 
 
